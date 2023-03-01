7a77959192f6fb64b634a45181107a5dd67713f8
pattnaikupasana <upasana.pattnaik@outlook.com>
2023-03-01 21:07:48 +0100

    added javascript-javascript1-week1 Homework

---
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -0,0 +1,23 @@
+let dogYearOfBirth = document.getElementById("birthYear").value;
+let dogYearFuture =  document.getElementById("futureYear").value;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = false;
+if (shouldShowResultInDogYears == true) {
+
+    dogYear *= 7;//dogYear=dogYear*7
+
+    //console.log(`Your dog will be ${dogYear} dog years old in 2027`)
+
+    document.getElementById("dogYearCalculation").innerHTML =`Your dog will be ${dogYear} dog years old in 2027`
+
+} 
+
+else {
+
+    //console.log(`Your dog will be ${dogYear} human years old in 2027`)
+
+    document.getElementById("dogYearCalculation").innerHTML = `Your dog will be ${dogYear} human years old in 2027`
+
+}
+
+
